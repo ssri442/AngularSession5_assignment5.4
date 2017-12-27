4ed3f80086fbe544ba16a9184ab56b9b11beb76b
Sai <33226057+ssri442@users.noreply.github.com>
2017-12-27 17:47:05 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,12 @@
+import { Component , ViewEncapsulation,OnInit} from '@angular/core';
+
+@Component({
+  selector: 'my-app-component',
+  templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css'],
+   
+})
+export class RootComponent {
+  title = 'app';
+   
+}
